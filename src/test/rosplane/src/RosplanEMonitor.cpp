dd483b8509c8ba68a862bbda8188c938cda5c8af
xyz <xueyuanzhou@qq.com>
2021-06-28 21:26:12 +0800

    添加了简易版监控节点 检测到动作失败后立即取消动作分派，重新生成问题并进行重规划和分派

---
+++ src/test/rosplane/src/RosplanEMonitor.cpp
@@ -0,0 +1,35 @@
+#include "RosplanEMonitor.h"
+
+namespace rosplane {
+    ROSPlanEMonitor::ROSPlanEMonitor() {
+        ros::NodeHandle nh("~");
+        _problem_generation_client = nh.serviceClient<std_srvs::Empty>("/rosplan_problem_interface/problem_generation_server");
+        _plan_client = nh.serviceClient<std_srvs::Empty>("/rosplan_planner_interface/planning_server");
+        _parse_client = nh.serviceClient<std_srvs::Empty>("/rosplan_parsing_interface/parse_plan");
+        _plan_dispatch_client = nh.serviceClient<rosplan_dispatch_msgs::DispatchService>("/rosplan_plan_dispatcher/dispatch_plan");
+        _plan_dispatch_cancel_client = nh.serviceClient<std_srvs::Empty>("/rosplan_plan_dispatcher/cancel_dispatch");
+    }
+    void ROSPlanEMonitor::action_feedback_callback(const rosplan_dispatch_msgs::ActionFeedback::ConstPtr& msg) {
+        if (msg->status == "action failed") {
+            ROS_ERROR("Rosplane replanning");
+            std_srvs::Empty empty;
+            _plan_dispatch_cancel_client.call(empty);
+            _problem_generation_client.call(empty);
+            _plan_client.call(empty);
+            _parse_client.call(empty);
+            rosplan_dispatch_msgs::DispatchService srv;
+            _plan_dispatch_client.call(srv);
+        }
+    }
+}
+
+
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "rosplane_monitor");
+    ros::NodeHandle nh("~");
+    rosplane::ROSPlanEMonitor monitor;
+    ros::Subscriber sub = nh.subscribe<rosplan_dispatch_msgs::ActionFeedback>("/rosplan_plan_dispatcher/action_feedback", 10, &rosplane::ROSPlanEMonitor::action_feedback_callback, &monitor);
+    ros::spin();
+    return 0;
+}
