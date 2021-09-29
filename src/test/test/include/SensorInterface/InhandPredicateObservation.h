cb985d4948729f1d952b4d2a721ebd1ca9d91ad1
bigpigeblow <425613268@qq.com>
2021-09-29 15:43:33 +0800

    将观察动作放入sensorAgent中

---
+++ src/test/test/include/SensorInterface/InhandPredicateObservation.h
@@ -0,0 +1,51 @@
+#include <SensorInterface/SensorInterface.h>
+
+// ros header
+#include <control_msgs/PointHeadActionGoal.h>
+#include <geometry_msgs/PointStamped.h>
+#include <sensor_msgs/image_encodings.h>
+#include <image_transport/image_transport.h>
+#include <tf/transform_listener.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2/buffer_core.h>
+#include <RPActionInterface.h>
+#include <std_srvs/Empty.h>
+#include <trajectory_msgs/JointTrajectory.h>
+#include <control_msgs/JointTrajectoryControllerState.h>
+
+// aruco header
+#include <aruco/aruco.h>
+
+// opencv header
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+namespace rosplane
+{
+    class InhandPredicateObervation : public SensorInterface{
+        public:
+            bool concreteCallback(const rosplane::SensorDispatch::ConstPtr& msg);
+            InhandPredicateObervation();
+
+
+        private:
+            void observeCallback(const sensor_msgs::ImageConstPtr& imgMsg);
+        
+            std::string imageTopic;
+            std::string windowName;
+            std::string armTopic;
+
+            ros::Publisher pub_head_topic;
+            ros::Publisher pub_arm_topic;
+
+            ros::Subscriber sub_arm_topic;
+
+            ros::ServiceServer service;
+
+            image_transport::Subscriber img_sub;
+            tf::TransformListener listener;
+            tf::StampedTransform transform;
+
+            control_msgs::PointHeadActionGoal head_action_goal;
+            trajectory_msgs::JointTrajectory arm_action_goal;
+    };
+} 
---
+++ src/test/test/src/SensorInterface/InhandPredicateObservation.cpp
@@ -0,0 +1,133 @@
+#include "SensorInterface/InhandPredicateObservation.h"
+
+namespace rosplane{
+    InhandPredicateObervation::InhandPredicateObervation(){
+        ros::NodeHandle nh("~");
+        pub_head_topic = nh.advertise<control_msgs::PointHeadActionGoal>("/head_controller/point_head_action/goal", 1);
+        pub_arm_topic = nh.advertise<trajectory_msgs::JointTrajectory>("/arm_controller/command", 1);
+        imageTopic = "/xtion/rgb/image_raw";
+        image_transport::TransportHints transportHints("compressed");
+        image_transport::ImageTransport it(nh);
+        img_sub = it.subscribe(imageTopic, 1, &InhandPredicateObervation::observeCallback, this, transportHints);
+        
+
+        head_action_goal.header.frame_id = "/base_link";
+        head_action_goal.goal.max_velocity = 1.0;
+        head_action_goal.goal.min_duration = ros::Duration(1.0);
+        head_action_goal.goal.target.header.frame_id = "/base_link";
+        head_action_goal.goal.pointing_axis.x = 1.0;
+        head_action_goal.goal.pointing_frame = "/head_2_link";
+
+        arm_action_goal.joint_names.push_back("arm_1_joint");
+        arm_action_goal.joint_names.push_back("arm_2_joint");
+        arm_action_goal.joint_names.push_back("arm_3_joint");
+        arm_action_goal.joint_names.push_back("arm_4_joint");
+        arm_action_goal.joint_names.push_back("arm_5_joint");
+        arm_action_goal.joint_names.push_back("arm_6_joint");
+        arm_action_goal.joint_names.push_back("arm_7_joint");
+    }
+
+    void InhandPredicateObervation::observeCallback(const sensor_msgs::ImageConstPtr& imgMsg){
+        cv_bridge::CvImagePtr cvImgPtr;
+        cvImgPtr = cv_bridge::toCvCopy(imgMsg, sensor_msgs::image_encodings::BGR8);
+        cv::imshow("test", cvImgPtr->image);
+        cv::waitKey(15);
+    }
+
+    bool InhandPredicateObervation::concreteCallback(const SensorDispatch::ConstPtr& msg){
+        ROS_INFO("(%s): concretCallback", pred_name.c_str());
+        // 检查手有没有拿到glass，拿到返回true，没拿到返回false
+
+        // ros nodehandle init
+        ros::NodeHandle nh;
+
+        // move head to look hand        
+        geometry_msgs::PoseStamped ps;
+        ros::Time time;
+        std::string err_string;
+        bool transform_ok = false;
+        ps.header.frame_id = "/gripper_link";
+        ps.header.stamp = ros::Time(listener.getLatestCommonTime("/base_link", "/gripper_link", time, &err_string));
+
+        while (!ros::isShuttingDown() && !transform_ok) {
+            try {
+                listener.waitForTransform("/base_link", "/gripper_link", ros::Time::now(), ros::Duration(1.0));
+                listener.lookupTransform("/base_link", "/gripper_link", ros::Time::now(), transform);
+                transform_ok = true;
+            }
+            catch (const std::exception& e) {
+                // std::cerr << e.what() << '\n';
+            }
+        }
+        head_action_goal.goal.target.point.x = transform.getOrigin().x();
+        head_action_goal.goal.target.point.y = transform.getOrigin().y();
+        head_action_goal.goal.target.point.z = transform.getOrigin().z();
+        pub_head_topic.publish(head_action_goal);
+        // TODO: 要实现先转头再转手，否则可能会先看到二维码
+        // 但是sleep好像会阻塞住整个ros（不一定）
+        ros::Duration(3.0).sleep();
+
+
+
+        // rotate wrist test
+        // get the arms' position
+        auto res = ros::topic::waitForMessage<control_msgs::JointTrajectoryControllerState>("/arm_controller/state");
+
+        // 
+
+        // first position
+        arm_action_goal.points.resize(2);
+        arm_action_goal.points[0].positions.resize(7);
+        arm_action_goal.points[0].positions[0] = res->actual.positions[0];
+        arm_action_goal.points[0].positions[1] = -0.21;
+        arm_action_goal.points[0].positions[2] = -2.20;
+        arm_action_goal.points[0].positions[3] = 1.16;
+        arm_action_goal.points[0].positions[4] = -0.71;
+        arm_action_goal.points[0].positions[5] = 1.11;
+        arm_action_goal.points[0].positions[6] = 2.0;
+        arm_action_goal.points[0].velocities.resize(7);
+        for (int i = 0; i < 7; i++) {
+            arm_action_goal.points[0].velocities[i] = 0.0;
+        }
+
+        arm_action_goal.points[0].time_from_start = ros::Duration(4.0);
+
+        // second position
+        arm_action_goal.points[1].positions.resize(7);
+        arm_action_goal.points[1].positions[0] = res->actual.positions[0];
+        arm_action_goal.points[1].positions[1] = -0.21;
+        arm_action_goal.points[1].positions[2] = -2.20;
+        arm_action_goal.points[1].positions[3] = 1.16;
+        arm_action_goal.points[1].positions[4] = -0.71;
+        arm_action_goal.points[1].positions[5] = 1.11;
+        arm_action_goal.points[1].positions[6] = -2.0;
+        arm_action_goal.points[1].velocities.resize(7);
+        for (int i = 0; i < 7; i++) {
+            arm_action_goal.points[1].velocities[i] = 0.0;
+        }
+
+        arm_action_goal.points[1].time_from_start = ros::Duration(8.0);
+        arm_action_goal.header.stamp = ros::Time::now();
+        pub_arm_topic.publish(arm_action_goal);
+
+        auto observe_res = ros::topic::waitForMessage<geometry_msgs::PoseStamped>("/aruco_single/pose", ros::Duration(8.0));
+        // ROS_ERROR("Where is bug");
+        auto now = ros::Time::now();
+        if (observe_res != nullptr && now - observe_res->header.stamp < ros::Duration(2.0)) {
+            ROS_INFO("%d, %d", observe_res->header.stamp.nsec, observe_res->header.stamp.sec);
+            ROS_INFO("%d, %d", now.nsec, now.sec);
+            return true;
+        }
+
+        return false;
+    }
+
+
+}
+
+int main(int argc, char** argv){
+    ros::init(argc, argv, "inhand_predicate_observe");
+    rosplane::InhandPredicateObervation ipo;
+    ipo.runSensorInterface();
+    return 0;
+}
